244632770650856f30ac2ba9efa0fa33cc064a64
johnsaunders17 <john.saunders17@outlook.com>
2025-08-08 16:42:36 +0100

    Create App.tsx

---
+++ frontend/src/frontend/src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ConstellationTravelHelper from './components/ConstellationTravelHelper'
+
+export default function App() {
+  return <ConstellationTravelHelper />
+}
